5d814d0938af9f324f9d8e28d175438daca01235
tanwe <tanwe@Sajid>
2022-08-30 11:03:31 +0530

    ChangesDone

--- src/test/java/SampleForGithub/UpdatedClass.java
+++ src/test/java/SampleForGithub/UpdatedClass.java
@@ -3,7 +3,9 @@ package SampleForGithub;
 public class UpdatedClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String BROWSER = System.getProperty("url");
+		String URL = System.getProperty("browser");
+		System.out.println("Browser====>"+BROWSER+" and the url====>"+URL);
 		
 		System.out.println("this is  a Updated Class");
 
